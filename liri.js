45edf243f9072e76c7fee6418fd319d8bc78ef17
R <merinogeospatial@gmail.com>
2018-06-22 19:59:18 -0400

    ready

--- liri.js
+++ liri.js
@@ -1,4 +1,11 @@
 require("dotenv").config();
+const keys = require("./keys");
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// const spotify = new Spotify(keys.spotify);
+// const client = new Twitter(keys.twitter);
+
+// Ask about the above declarations... do I need to use new? Looks like I can access keys like the logging below?
+
+console.log(keys.spotify);
+
+console.log(keys.twitter);
