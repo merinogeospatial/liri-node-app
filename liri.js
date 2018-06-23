8bad064da4d151b697072eabc3e8178a0d0bea40
R <merinogeospatial@gmail.com>
2018-06-22 21:59:02 -0400

    tied twitter and spotify, verified working with keys

--- liri.js
+++ liri.js
@@ -1,11 +1,35 @@
+// Dependencies
 require("dotenv").config();
+const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
 const keys = require("./keys");
 
 // const spotify = new Spotify(keys.spotify);
 // const client = new Twitter(keys.twitter);
 
-// Ask about the above declarations... do I need to use new? Looks like I can access keys like the logging below?
 
 console.log(keys.spotify);
-
 console.log(keys.twitter);
+
+ // Twitter Handling 
+const client = new Twitter(
+  keys.twitter
+);
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+// Spotify Handling
+const spotify = new Spotify(keys.spotify);
+
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
