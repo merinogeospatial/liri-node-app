4f8c50679f49962456accf575f0ca5fe60ed644c
R <merinogeospatial@gmail.com>
2018-06-23 20:35:19 -0400

    add in inquirer and require

--- keys.js
+++ keys.js
@@ -1,3 +1,4 @@
+
 console.log('Keys loaded!');
 
 exports.twitter = {
--- liri.js
+++ liri.js
@@ -2,14 +2,51 @@
 require("dotenv").config();
 const Twitter = require('twitter');
 const Spotify = require('node-spotify-api');
+const inquirer = require("inquirer");
+const request = require("request");
 const keys = require("./keys");
 
-// const spotify = new Spotify(keys.spotify);
-// const client = new Twitter(keys.twitter);
+
+// User interaction with acquirer
+// Prompt the user to provide location information.
+inquirer.prompt([
+
+  {
+    type: "list",
+    name: "action",
+    message: "Hola, Liri-bot here! What would you like to do?",
+    choices: ["Lookup Tweets", "Lookup a Song", "Lookup a Movie", "Surprise me!"]
+  },
+
+  {
+    type: "input",
+    name: "parameter",
+    message: "Please type in the name the song or movie you would like to search! If you're not searching for anything just press 'enter'."
+  }
+
+// After the prompt, store the user's response in a variable called location.
+]).then(function(choice) {
+  
+  if (choice.action === "Lookup Tweets") {
+    // Look up tweets here using Twitter api
+  }
+  else if (choice.action === "Lookup a Song") {
+    // Lookup song using Spotify api
+  }
+  else if (choice.action === "Lookup a Movie") {
+    // Lookup movie using omdb (this is a request method!)
+    request('http://www.omdbapi.com/?apikey=trilogy&t=the incredibles', function (error, response, body) {
+      console.log('error:', error); 
+      console.log('statusCode:', response && response.statusCode); 
+      console.log('body:', body); 
+    });
+
+  }
 
 
-console.log(keys.spotify);
-console.log(keys.twitter);
+});
+
+
 
  // Twitter Handling 
 const client = new Twitter(
@@ -19,7 +56,7 @@ const client = new Twitter(
 var params = {screen_name: 'nodejs'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets);
+    // console.log(tweets);
   }
 });
 
@@ -28,8 +65,8 @@ const spotify = new Spotify(keys.spotify);
 
 spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
     if (err) {
-      return console.log('Error occurred: ' + err);
+      // return console.log('Error occurred: ' + err);
     }
    
-  console.log(data); 
+  // console.log(data); 
   });
